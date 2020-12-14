608db8e3838241d3261d381c4fcaa6ee7e3cb1f1
SquareWhite <olaha66@gmail.com>
2020-12-14 20:34:23 +0300

    Add elsatic circle collisions for puck

--- src/controller/game-logic/collisions/circle.ts
+++ src/controller/game-logic/collisions/circle.ts
@@ -5,7 +5,10 @@ import {
     projectPointToLine,
     getDirection,
     movePointInDirection,
-    Direction
+    Direction,
+    intersectLineWithCircle,
+    Circle,
+    Line
 } from '../../../helpers/math';
 import { GameCircle } from '../../../model/types';
 import {
@@ -63,19 +66,40 @@ export const resolveCircleCollision = (
     collision: CircleCollision
 ): [IdentifiedPoint, Direction] => {
     const { circle, object: otherCircle } = collision;
-    const newDirection = circle.movement.directionVector;
     const id = circle.position.id;
 
+    let newPosition: Point;
+    let newDirection = circle.movement.directionVector;
+    if (!collision.isElastic) {
+        [newPosition, newDirection] = _pushCircleOutOfTheOtherCircle(
+            circle,
+            otherCircle,
+            collision.type
+        );
+    } else {
+        [newPosition, newDirection] = _bounceCircleOffOtherCircle(
+            circle,
+            otherCircle
+        );
+    }
+
+    return [{ ...newPosition, id }, newDirection];
+};
+
+const _pushCircleOutOfTheOtherCircle = (
+    circle: GameCircle,
+    otherCircle: IdentifiedCircle,
+    type: CircleCollision['type']
+): [Point, Direction] => {
     const outOfTheCircle = getDirection(otherCircle.position, circle.position);
     const minDistance = circle.radius + otherCircle.radius;
     const currentDistance = calculateDistance(
         circle.position,
         otherCircle.position
     );
-
+    const newDirection = circle.movement.directionVector;
     let newPosition: Point;
-
-    if (collision.type === 'CIRCLE_CROSS') {
+    if (type === 'CIRCLE_CROSS') {
         const lineBetweenCircles = {
             point1: circle.previousPosition,
             point2: otherCircle.position
@@ -109,8 +133,60 @@ export const resolveCircleCollision = (
             minDistance - currentDistance
         );
     }
+    return [newPosition, newDirection];
+};
 
-    return [{ ...newPosition, id }, newDirection];
+const _bounceCircleOffOtherCircle = (
+    circle: GameCircle,
+    otherCircle: IdentifiedCircle
+): [Point, Direction] => {
+    const travelPath: Line = {
+        point1: circle.previousPosition,
+        point2: circle.position
+    };
+    const travelDistance = calculateDistance(
+        circle.previousPosition,
+        circle.position
+    );
+
+    const collisionBorder: Circle = {
+        position: otherCircle.position,
+        radius: otherCircle.radius + circle.radius
+    };
+    const impactPoints = intersectLineWithCircle(travelPath, collisionBorder);
+    if (impactPoints.length < 1) {
+        throw new Error("Couldn't find any impact points!");
+    }
+    const impactPoint = impactPoints.find((point: Point) => {
+        const prevPosToImpact = calculateDistance(
+            circle.previousPosition,
+            point
+        );
+        const impactToPos = calculateDistance(point, circle.position);
+        return prevPosToImpact + impactToPos - travelDistance < 10e-6;
+    });
+    if (!impactPoint) {
+        throw new Error("Couldn't find any impact points!");
+    }
+    const centerToImpactLine = {
+        point1: otherCircle.position,
+        point2: impactPoint
+    };
+    const pointOnALine = projectPointToLine(
+        circle.position,
+        centerToImpactLine
+    );
+    const distance = Math.max(
+        calculateDistance(pointOnALine, impactPoint),
+        circle.radius
+    );
+    const newPosition = movePointInDirection(
+        circle.position,
+        getDirection(pointOnALine, impactPoint),
+        distance
+    );
+    const newDirection = getDirection(impactPoint, newPosition);
+    return [newPosition, newDirection];
 };
 
 const _collidesWithCircle = (
